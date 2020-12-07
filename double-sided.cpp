540e1f74e77211ad785065df57db96f3c87bb73e
Sacha Jungerman <sjungerman@wisc.edu>
2020-12-07 08:40:15 -0600

    cleanup

--- double-sided.cpp
+++ double-sided.cpp
@@ -10,24 +10,20 @@ int main (int argc, char *argv[]) {
   const int N = atoi(argv[1]);
   const int M = atoi(argv[2]); 
   char X[N];
-  int pad[100];
   int i;
-  int pad2[100];
 
-  for (i = 1; i < N-1; ++i)
+  for (i = 0; i < N; ++i)
       X[i] = 2;
 
   // Calculate original sum
   long int sum = 0;
-  int pad3[100];
-  for (i = 1; i < N-1; ++i)
+  for (i = 0; i < N; ++i)
     sum += X[i];
   printf("Original sum: %ld\n", sum);
 
   // Perform attack
   char value1 = X[N/2-1];
   char value2 = X[N/2+1]; 
-  int pad5[100];
   for (i = 0; i < M; i++) {
     //printf("i = %d\n", i);
     X[N/2-1] = 3;
@@ -38,8 +34,7 @@ int main (int argc, char *argv[]) {
 
   // Check new sum
   long int new_sum = 0;
-  int pad4[100];
-  for (int i = 1; i < N-1; ++i)
+  for (int i = 0; i < N; ++i)
     new_sum += X[i];
   printf("Sum after attack: %ld\n", new_sum);
 
